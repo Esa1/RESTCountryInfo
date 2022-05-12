4847732ea91e71db88241f7d3923a5b8ba237468
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-12 20:05:12 +0300

    Country name, currency, capital shown for Finland

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import React from "react"
 import Main from "./Components/Main"
 import Header from "./Components/Header"
 
-export default function App() {
+export default function App(props) {
+//    console.log(props.data[0].capital)
     return (
         <div className="App">
             <Header />
             <hr/>
-            <Main />
+            <Main 
+                name={props.data[0].name.common}
+                currency={props.data[0].currencies}
+                capital={props.data[0].capital}
+            />
         </div>
         
     )
--- Components/Main.js
+++ Components/Main.js
@@ -1,7 +1,8 @@
 import React from "react"
 //import Place from "./Place"
-//import ShowCountryInfo from "./ShowCountryInfo"
-//import RetrieveData from "./RetrieveData"
+//import data from "../data"
+import ShowCountryInfo from "./ShowCountryInfo"
+import RetrieveData from "./RetrieveData"
 
 /*function ShowData() {
     let url = "https://restcountries.com/v3.1/name/finland?fullText=true";
@@ -10,9 +11,35 @@ import React from "react"
     })
 }*/
 
-export default function Main() {
+export default function Main(props) {
 //    ShowData();
+    console.log(Object.keys(props.capital))
     return (
-        <div>Main</div>
+        <div className="main">capital
+            <div>Country: {props.name}</div>
+            <div>Currency: {Object.keys(props.currency)[0]}</div>
+            <div>Capital: {props.capital}</div>
+        </div>
     )
+/*    
+            <div>currency: {props.currency}</div>
+
+return (
+        <RetrieveData />
+    )*/
+/*    return (
+        <div>
+            Main
+            <ShowCountryInfo />
+        </div>
+        <div className="Main">
+            {data.map(place =>
+                <Place
+                    key={place.title}
+                    p={place}
+                />
+            )}
+        </div>
+
+    )*/
 }
