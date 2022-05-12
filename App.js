d6fbbd65876aa68c552c9d2e057fb742098f2f5c
Esa <esa.torkkila@gmail.com>
2022-05-12 19:01:33 +0300

    Add files via upload
    REST API fetch fetch works.

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Main from "./Components/Main"
+import Header from "./Components/Header"
+
+export default function App() {
+    return (
+        <div className="App">
+            <Header />
+            <hr/>
+            <Main />
+        </div>
+        
+    )
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+/*
+FULL NAME
+Search by country full name
+https://restcountries.com/v3.1/name/{name}?fullText=true
+https://restcountries.com/v3.1/name/aruba?fullText=true
+*/
+function ShowData() {
+    let url = "https://restcountries.com/v3.1/name/finland?fullText=true";
+    $.getJSON(url, function(data, status) {
+        console.log(data)
+        ReactDOM.render(<App data={data}/>, document.getElementById("root"))
+   })
+}
+ShowData()
+//ReactDOM.render(<App />, document.getElementById("root"))
