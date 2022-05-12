1723b063a74a11bfe1db93b3da7a6fb57f5cfd78
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-12 23:33:16 +0300

    FI, SW, NO work by selection.

--- Components/Main.js
+++ Components/Main.js
@@ -3,13 +3,14 @@ import { useState } from 'react';
 import ShowCountryInfo from "./ShowCountryInfo"
 //import RetrieveData from "./RetrieveData"
 
-function RetrieveData(country) {
+/*function RetrieveData(country) {
     console.log(country)
+    setCountry(country)
     let url = "https://restcountries.com/v3.1/name/" + country + "?fullText=true";
     $.getJSON(url, function(data, status) {
         console.log(data)
     })
-}
+}*/
 
 export default function Main(props) {
 //    ShowData();
@@ -20,11 +21,32 @@ export default function Main(props) {
         capital: 'CInit'
     });
     
-    console.log(Object.keys(props.capital))
+ /*            <div className="main--countryinfo">
+                <div>Country: {props.name}</div>
+                <div>Currency: {Object.keys(props.currency)[0]}</div>
+                <div>Capital: {props.capital}</div>
+            </div>*/   
+    function RetrieveData(country) {
+        console.log(country)
+        setCountry(country)
+        let url = "https://restcountries.com/v3.1/name/" + country + "?fullText=true";
+        $.getJSON(url, function(data, status) {
+            console.log(data[0])
+            console.log(Object.keys(data[0].currencies)[0])
+            setInfo({
+                country: data[0].name.common,
+                currency: Object.keys(data[0].currencies)[0],
+                capital: data[0].capital
+            })
+        })
+    }
+
+//    console.log(Object.keys(props.capital))
     return (
         <div className="main">
             <div className="main--selection">
                 <select
+                    value={country}
                     onChange={e => RetrieveData(e.target.value)}
                 >
                     <option value="Select">Select</option>
@@ -45,4 +67,5 @@ export default function Main(props) {
             </div>
         </div>
     )
+
 }
